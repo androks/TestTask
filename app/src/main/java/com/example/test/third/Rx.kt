5877913e4ac25377c030861c2b5e2194176da747
Bohdan Oliinyk <bohdan.olynyk@gmail.com>
2021-02-27 17:51:18 +0200

    third task

--- app/src/main/java/com/example/test/third/Rx.kt
+++ app/src/main/java/com/example/test/third/Rx.kt
@@ -6,9 +6,18 @@ import java.util.concurrent.TimeUnit
 
 fun rxTask() {
     Observable.intervalRange(0, Long.MAX_VALUE, 0, 1L, TimeUnit.SECONDS)
-        .doOnEach { Log.d("HELLO", "each ${it.value}") }
+        .doOnEach { Log.d("RX", "emit ${it.value}") }
         .filter { it % 3 == 0L || it % 5 == 0L }
-        .subscribe {
-            Log.d("HELLO", "result $it")
+        .map { it % 5 == 0L }
+        .distinctUntilChanged()
+        .switchMap {
+            if (it) {
+                Observable.just(it)
+            } else {
+                Observable.just(it)
+                    .delay(3500L, TimeUnit.MILLISECONDS)
+            }
+        }.subscribe {
+            Log.d("RX", "connected $it")
         }
 }
